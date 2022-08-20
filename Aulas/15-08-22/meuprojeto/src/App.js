8753ebdc8106971724655146b0d8f51126c35154
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2022-08-19 21:56:53 -0300

    feat: cria lista post

--- Aulas/15-08-22/meuprojeto/src/App.js
+++ Aulas/15-08-22/meuprojeto/src/App.js
@@ -9,7 +9,33 @@ import Post from "./components/Post";
 import Doces from "./components/Doces";
 
 function App() {
-  const docesEmCasa = ["alpino", "sensação", "shot"];
+  /* const docesEmCasa = ["alpino", "sensação", "shot"]; */
+  const posts = [
+    {
+      id: 1,
+      otoDoPerfil:
+        "https://cdn.pensador.com/img/authors/ho/me/homer-simpson-l.jpg",
+      tempoDoPost: "hoje",
+      nomeDoUsuario: "Otávio",
+      descricaoPost: "lorem ips...",
+      imagemPublicada:
+        "https://www.ahempreendimentos.com.br/images/jardim-curitiba-02.jpg",
+      shareUrl: "www.google.com",
+      curtidoPor: 25,
+    },
+    {
+      id: 2,
+      fotoDoPerfil:
+        "https://cdn.pensador.com/img/authors/ho/me/homer-simpson-l.jpg",
+      tempoDoPost: "hoje",
+      nomeDoUsuario: "Sebas",
+      descricaoPost: "lorem ips...",
+      imagemPublicada:
+        "https://www.ahempreendimentos.com.br/images/jardim-curitiba-02.jpg",
+      shareUrl: "www.google.com",
+      curtidoPor: 0,
+    },
+  ];
   return (
     <div>
       {/* <Titulo />
@@ -17,17 +43,23 @@ function App() {
       <Soma numero1={10} numero2={20} />
       <CapsLock2 textoEnviado = "Sou uma props"/> 
       <button textoEnviado="Botão Azul" corFundo="blue"></button>
-      <CapsLock3> Eu sou um filho</CapsLock3>
-      <Post
-        fotoDoPerfil="https://cdn.pensador.com/img/authors/ho/me/homer-simpson-l.jpg"
-        tempoDoPost={10}
-        nomeDoUsuario="Otávio"
-        descricaoPost="lorem ips..."
-        imagemPublicada="https://www.ahempreendimentos.com.br/images/jardim-curitiba-02.jpg"
-        shareUrl="www.google.com"
-        curtidoPor={25}
-      /> */}
-      <Doces listaDeDoces={docesEmCasa} />
+      <CapsLock3> Eu sou um filho</CapsLock3> */}
+      {posts.map((post) => {
+        return (
+          <Post
+            key={post.id}
+            fotoDoPerfil={post.fotoDoPerfil}
+            tempoDoPost={post.tempoDoPost}
+            nomeDoUsuario={post.nomeDoUsuario}
+            descricaoPost={post.descricaoPost}
+            imagemPublicada={post.imagemPublicada}
+            shareUrl={post.shareUrl}
+            curtidoPor={post.curtidoPor}
+          />
+        );
+      })}
+
+      {/* <Doces listaDeDoces={docesEmCasa} /> */}
     </div>
   );
 }
